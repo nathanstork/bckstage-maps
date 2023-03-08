c2f02064c0d80b21ea2fc6e9a5a3325ad4b72b75
Nathan Stork <nejc.stork@hotmail.nl>
2023-03-08 18:38:04 +0100

    Prettier config + code formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,24 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
+const prettierRC = require("./.prettierrc.json");
 
 module.exports = {
-  root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    root: true,
+    extends: [
+        "plugin:vue/vue3-essential",
+        "eslint:recommended",
+        "@vue/eslint-config-typescript",
+        "@vue/eslint-config-prettier/skip-formatting"
+    ],
+    parserOptions: {
+        ecmaVersion: "latest"
+    },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          ...prettierRC,
+        }
+      ]
+    }
+};
