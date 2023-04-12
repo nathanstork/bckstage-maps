6f4b62a6ac9f6bfcfd0f0c4f103258af049492f7
Thijs Melis <t.melis@student.fontys.nl>
2023-04-12 10:40:18 +0200

    Landingspage start

---
+++ src/components/Landingspage.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="container mx-auto">
+        <header>
+            <nav class="w-full">
+                <div class="logo">
+                    <img src="src\assets\logobackstagemaps.svg" />
+                    <span>Backstagemaps</span>
+                </div>
+            </nav>
+        </header>
+        <main></main>
+        <footer></footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "LandingPage"
+};
+</script>
+
+<style>
+nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 50px;
+    top: 25px;
+    color: #fff;
+}
+
+.logo {
+    position: relative;
+    width: 60px;
+}
+
+.logo img {
+    position: absolute;
+    z-index: 1;
+}
+
+.logo span {
+    position: relative;
+    z-index: 2;
+    margin-left: 70px;
+    font-size: 30px;
+}
+</style>
