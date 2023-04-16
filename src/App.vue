704a80c47e90d4277fdb45685a3cff4f42e02c6e
Thijs Melis <t.melis@student.fontys.nl>
2023-04-16 10:56:54 +0200

    Standard layout added

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,18 @@
     <header>
         <nav class="container text-center">
             <div class="row">
-                <div class="col-12">
-                    <img src="src\assets\logobackstagemaps.svg" style="width: 100px !important" />
-                    <h1>Backstagemaps</h1>
+                <div class="col-1">Gear for Settings</div>
+                <div class="col-10 d-flex align-items-center justify-content-center">
+                    <img
+                        src="src\assets\logobackstagemaps.svg"
+                        style="width: 70px !important; margin-top: 10px"
+                    />
+                    <h1 style="margin-left: 20px; margin-top: 10px">Backstagemaps</h1>
                 </div>
+                <div class="col-1">x</div>
             </div>
         </nav>
     </header>
-
     <main>
         <router-view />
     </main>
---
+++ src/components/Adminmenu.vue
@@ -0,0 +1,1 @@
+<template>Create a Gear for settings menu</template>
