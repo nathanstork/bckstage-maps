9c5df61e857772fa76e70aba38bd74c450265000
Thijs Melis <t.melis@student.fontys.nl>
2023-04-16 11:10:56 +0200

    standard layout - fa fa-cog

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
 <template>
+    <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+    />
     <header>
         <nav class="container text-center">
             <div class="row">
-                <div class="col-1">Gear for Settings</div>
+                <div class="col-1">
+                    <i
+                        class="fa fa-cog"
+                        style="font-size: 40px; color: #0096ff; margin-top: 15px"
+                    ></i>
+                </div>
                 <div class="col-10 d-flex align-items-center justify-content-center">
                     <img
                         src="src\assets\logobackstagemaps.svg"
@@ -10,7 +19,6 @@
                     />
                     <h1 style="margin-left: 20px; margin-top: 10px">Backstagemaps</h1>
                 </div>
-                <div class="col-1">x</div>
             </div>
         </nav>
     </header>
