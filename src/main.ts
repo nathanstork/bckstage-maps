f11697217cbfe81b67006247f6f8ba6fb3f4bd71
Nathan Stork <nejc.stork@hotmail.nl>
2023-05-15 21:58:31 +0200

    Vue Query installation

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "@/router";
+import { VueQueryPlugin } from "vue-query";
 
 import "./assets/main.css";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(VueQueryPlugin).mount("#app");
