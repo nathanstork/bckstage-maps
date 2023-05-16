de8888bbc239df0e2380db26212a912e545cb240
Nathan Stork <nejc.stork@hotmail.nl>
2023-05-16 18:37:02 +0200

    Code formatting + removed unnecessary import in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,5 @@ import router from "@/router";
 import { VueQueryPlugin } from "vue-query";
 
 import "./assets/main.css";
-import store from "@/store";
 
 createApp(App).use(router).use(VueQueryPlugin).mount("#app");
