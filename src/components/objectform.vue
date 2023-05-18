eadaef9bc91b552d2483475f5d6198c847eaaed4
Thijs Melis <t.melis@student.fontys.nl>
2023-05-18 17:45:13 +0200

    added hide/show option

--- src/components/objectform.vue
+++ src/components/objectform.vue
@@ -3,17 +3,31 @@
         <button type="button" class="eyeopen" @click="expandTable">
             <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
+                width="18"
+                height="18"
                 fill="currentColor"
-                class="bi bi-eye"
+                class="bi bi-eye-slash-fill"
                 viewBox="0 0 16 16"
             >
                 <path
-                    d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"
+                    v-if="eyeIcon === 'bi bi-eye'"
+                    d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"
                 />
                 <path
-                    d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"
+                    d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"
+                />
+
+                <path
+                    v-if="eyeIcon === 'bi-eye-slash'"
+                    d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"
+                />
+                <path
+                    v-else
+                    d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0.0 0 2.829 2.829z"
+                />
+                <path
+                    v-else
+                    d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"
                 />
             </svg>
         </button>
@@ -77,7 +91,7 @@
             </tbody>
         </table>
     </div>
-    <div class="buttons">
+    <div class="buttons" v-if="!isTableExpanded">
         <button
             type="button"
             class="btn btn-ambu"
@@ -187,7 +201,7 @@ body {
 }
 
 .eyeopen {
-    margin-left: 200px;
+    margin-left: 187px;
     cursor: pointer;
 }
 
@@ -251,8 +265,10 @@ export default {
     data() {
         return {
             tableHeight: "390px",
+            isTableExpanded: false,
             showMenu: null,
             showCustomScreen: false,
+            eyeIcon: "bi-eye",
             newObject: {
                 object: "",
                 color: "",
@@ -285,12 +301,17 @@ export default {
             return this.items.filter(item => {
                 return item.object.toLowerCase().includes(this.filterText.toLowerCase());
             });
+        },
+        isTableExpanded() {
+            return this.tableHeight === "80vh";
         }
     },
 
     methods: {
         expandTable() {
-            this.tableHeight = "100vh";
+            this.tableHeight = this.isTableExpanded ? "390px" : "80vh";
+            this.isTableExpanded = !this.isTableExpanded;
+            this.eyeIcon = this.isTableExpanded ? "bi-eye-slash" : "bi bi-eye";
         },
 
         addItemWithColor(object, color, deleteName) {
