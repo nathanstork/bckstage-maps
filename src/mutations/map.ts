4d0fdffe09678a9af1c19691bb8589348571b359
Nathan Stork <nejc.stork@hotmail.nl>
2023-05-29 23:37:53 +0200

    Replace current map with newly uploaded map
    After a successful upload, all other files in the folder are removed.
    Also, added a UUID to the file name to prevent an error from Supabase.

--- src/mutations/map.ts
+++ src/mutations/map.ts
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/vue-query";
 import { supabase } from "@/lib/supabaseClient";
+import { uuid } from "vue-uuid";
 
 export const useMapMutation = (id: string) => {
     const queryClient = useQueryClient();
@@ -7,10 +8,41 @@ export const useMapMutation = (id: string) => {
     return useMutation(
         async (file: File) =>
             new Promise((resolve, reject) => {
+                const fileExtension = file.name.split(".").pop();
+
+                if (!fileExtension) {
+                    reject("No file extension found");
+                    return;
+                }
+
+                // Add a UUID to the file name to prevent a duplicate error
+                const fileName = `${file.name.replace(
+                    `.${fileExtension}`,
+                    ""
+                )}-${uuid.v4()}.${fileExtension}`;
+
                 supabase.storage
                     .from("maps")
-                    .upload(`${id}/${file.name}`, file, {
+                    .upload(`${id}/${fileName}`, file, {
                         cacheControl: "3600"
+                    })
+                    .then(() => {
+                        // Get all files in event folder
+                        supabase.storage
+                            .from("maps")
+                            .list(id)
+                            .then(res => {
+                                // Delete all files except for the just uploaded one
+                                const filesToRemove = res.data
+                                    ?.filter(f => f.name !== fileName)
+                                    ?.map(f => `${id}/${f.name}`);
+
+                                if (filesToRemove)
+                                    supabase.storage
+                                        .from("maps")
+                                        .remove(filesToRemove)
+                                        .catch(reject);
+                            });
                     })
                     .catch(reject);
             }),
