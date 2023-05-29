11c000f4a431d109de812309ff9584e5be4210e5
Nathan Stork <nejc.stork@hotmail.nl>
2023-05-29 21:47:02 +0200

    Renamed Not Found view

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import EventsOverview from "@/views/EventsOverview.vue";
 import EventCreate from "@/views/EventCreate.vue";
-import NotFound from "@/views/NotFound.vue";
+import NotFound from "@/views/NotFoundView.vue";
 import store from "@/store";
 
 const checks = {
